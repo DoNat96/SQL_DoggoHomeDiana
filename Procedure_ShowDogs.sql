7b2e2fdb2d0c102fae06eda65ef0ce9c80a9d666
DoNat96 <DonDoNat96@users.noreply.github.com>
2020-07-10 19:03:24 +0200

    Procedure_Show_Dogs
    Procedure allows finding a dog of a certain breed and age

---
+++ Procedure_ShowDogs.sql
@@ -0,0 +1,17 @@
+create procedure ShowDogs
+@breed nvarchar(100),@gender char(1), @min_age int, @max_age int
+as
+begin
+
+select D.dog_name as Name, D.gender as Gender, B.breed_name as Breed, D.history as History, D.boxID, X.age as BoxName,
+DATEDIFF(YEAR,D.born,GETDATE()) as AgeNow
+from Dogs as D
+join DogBreed as DB on DB.id_dog=D.id_dog
+join D_Breed as B on B.id_breed=DB.id_breed
+join Boxes as X on X.id_box=D.boxID
+where B.breed_name=@breed and D.gender=@gender and (DATEDIFF(YEAR,D.born,GETDATE()) between @min_age AND @max_age)
+
+end
+GO
+
+exec ShowDogs Husky, 'F', 1, 6
