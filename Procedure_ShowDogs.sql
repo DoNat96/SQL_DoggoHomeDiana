faa1783d86802d76cc8b204458bb11a021742da0
DoNat96 <DonDoNat96@users.noreply.github.com>
2020-07-11 20:02:46 +0200

    Alter procedure ShowDogs
    Alter procedure ShowDogs
    no need to specify breed

--- Procedure_ShowDogs.sql
+++ Procedure_ShowDogs.sql
@@ -1,8 +1,9 @@
-create procedure ShowDogs
-@breed nvarchar(100),@gender char(1), @min_age int, @max_age int
+alter procedure ShowDogs
+@breed nvarchar(100)=null,@gender char(1), @min_age int, @max_age int
 as
 begin
 
+if @breed is not null
 select D.dog_name as Name, D.gender as Gender, B.breed_name as Breed, D.history as History, D.boxID, X.age as BoxName,
 DATEDIFF(YEAR,D.born,GETDATE()) as AgeNow
 from Dogs as D
@@ -10,6 +11,16 @@ join DogBreed as DB on DB.id_dog=D.id_dog
 join D_Breed as B on B.id_breed=DB.id_breed
 join Boxes as X on X.id_box=D.boxID
 where B.breed_name=@breed and D.gender=@gender and (DATEDIFF(YEAR,D.born,GETDATE()) between @min_age AND @max_age)
+
+else
+select D.dog_name as Name, D.gender as Gender, B.breed_name as Breed, D.history as History, D.boxID, X.age as BoxName,
+DATEDIFF(YEAR,D.born,GETDATE()) as AgeNow
+from Dogs as D
+join DogBreed as DB on DB.id_dog=D.id_dog
+join D_Breed as B on B.id_breed=DB.id_breed
+join Boxes as X on X.id_box=D.boxID
+where D.gender=@gender and (DATEDIFF(YEAR,D.born,GETDATE()) between @min_age AND @max_age)
+
 
 end
 GO
